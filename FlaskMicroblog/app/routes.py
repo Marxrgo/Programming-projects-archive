af5ca441205587e222c49029d4ba98610a157786
Marvens Longchamp <Longchampmarvens0@gmail.com>
2026-07-30 17:10:41 -0400

    More work on blog app up to middle of chap 3

--- FlaskMicroblog/app/routes.py
+++ FlaskMicroblog/app/routes.py
@@ -1,6 +1,41 @@
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username':'Mar'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Samuel'},
+            'body': 'I love Flask!'
+        },
+        {
+            'author': {'username': 'Scott'},
+            'body': 'Maryland is the greatest!'
+        },
+        {
+            'author': {'username': 'Jean'},
+            'body': 'Beautiful day in FL!'
+        },
+]
+
+    return render_template('index.html', title = 'home', user = user, posts = posts)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {form.username.data}, remeber_me={form.remember_me.data}')
+        return redirect('/index')
+
+    return render_template('login.html', title = 'Sign in', form=form)
